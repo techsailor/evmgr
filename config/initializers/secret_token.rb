32b2963f0bb6e5473804b1a0f362e1726f1d9181
clanglois <christian@redtoad.co.uk>
2014-08-30 13:15:05 +0100

    Initial import rails new evmgr;bundle install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evmgr::Application.config.secret_token = 'cdc30db38dc3a40583101190eb3083c86e227cb10597febd978b982ebcb9374de0948e7e83466738e6a422bfcf6c8333dfefeb651bcbf3334787edc87e041f00'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Evmgr::Application.config.session_store :cookie_store, key: '_evmgr_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Evmgr::Application.config.session_store :active_record_store
